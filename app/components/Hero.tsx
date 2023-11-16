a1086744b87f28b782dc3b1dccd96458a1fc27ff
Ose Ogbe <therealnylat@gmail.com>
2023-11-16 16:50:12 +0100

    minor fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
                 <div className="max-w-7xl mx-auto p-4 lg:p-0">
                     <div className="text-center">
                         <h1 className="text-4xl lg:text-7xl font-bold text-[#E3D101] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">Discover the Allure of Paradise <br className="hidden lg:block" />with Palm Trees</h1>
-                        <p className="mt-6 text-lg lg:text-2xl text-white md:drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">Discover the glamour and elegance of the tropics with our extensive selection of stunning palm trees, <br className="hidden lg:block" />perfectly suited for any landscape or garden project.</p>
+                        <p className="mt-6 text-lg lg:text-2xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">Discover the glamour and elegance of the tropics with our extensive selection of stunning palm trees, <br className="hidden lg:block" />perfectly suited for any landscape or garden project.</p>
                         <div className="mt-8 flex items-center justify-center space-x-4">
                             <button className="bg-primary text-white text-md lg:text-2xl font-semibold px-8 py-4 lg:px-12 lg:py-5 rounded lg:rounded-lg z-10" onClick={() => router.push('/palm-species')}>Explore Now</button>
                             <button className="bg-transparent text-[#E3D101] border border-[#E3D101] inner-border text-md lg:text-2xl font-semibold px-8 py-4 lg:px-12 lg:py-5 rounded lg:rounded-lg z-10" onClick={() => router.push('/contact-us')}>Contact Us</button>
