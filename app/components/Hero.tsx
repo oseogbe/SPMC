65212af2751039a08323be681b4688f81452ea6f
Osememen Ogbe <osememen.ogbe@gmail.com>
2024-11-25 10:51:21 +0100

    button ui change

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
                         <p className="mt-6 text-lg lg:text-2xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">Discover the glamour and elegance of the tropics with our extensive selection of stunning palm trees, <br className="hidden lg:block" />perfectly suited for any landscape or garden project.</p>
                         <div className="mt-8 flex items-center justify-center space-x-4">
                             <button className="bg-primary text-white text-md lg:text-2xl font-semibold px-8 py-4 lg:px-12 lg:py-5 rounded lg:rounded-lg z-10" onClick={() => router.push('/palm-species')}>Explore Now</button>
-                            <button className="bg-transparent text-[#E3D101] border border-[#E3D101] inner-border text-md lg:text-2xl font-semibold px-8 py-4 lg:px-12 lg:py-5 rounded lg:rounded-lg z-10" onClick={() => router.push('/contact-us')}>Contact Us</button>
+                            <button className="bg-white text-primary border border-primary inner-border text-md lg:text-2xl font-semibold px-8 py-4 lg:px-12 lg:py-5 rounded lg:rounded-lg z-10" onClick={() => router.push('/contact-us')}>Contact Us</button>
                         </div>
                     </div>
                 </div>
